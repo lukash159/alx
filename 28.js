8906787f469c956b9c6e49c710becaed2da7e1ae
Lukasz <lukash159@o2.pl>
2019-07-27 16:54:14 +0200

    sobota 2707

---
+++ 28.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+//let x = document.querySelectorAll("li");
+//x[0].style.backgroundColor="red";
+//x[2].style.backgroundColor="red";
+
+//elem = document.querySelector("#test");
+//console.log(elem.innerHTML);
+//elem.innerHTML="BYE"; //wstawia ze znacznikami
+//elem.innerText="<i>BYE</i>"; //pobiera tylko txt i wstawia tylko txt
+//console.log(elem.tagName);
+//elem.setAttribute("align","center");
+//elem.setAttribute("class", "kolor");
+
+//document.querySelector("#poleInput").value;
+
+elem=document.querySelector("#imie");
+alert(elem.value)
+
+});
